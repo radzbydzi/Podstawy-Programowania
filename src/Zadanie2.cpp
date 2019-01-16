0a5852e0c7b6fb1edea626943c98d0b279f8f28a
Weronika <Weronika>
2019-01-16 16:36:00 +0100

    Kody ASCII do liter

--- src/Zadanie2.cpp
+++ src/Zadanie2.cpp
@@ -14,6 +14,11 @@ Zadanie2::~Zadanie2()
 }
 void Zadanie2::sprawdzZnak(char znak)
 {
+ //a - 97 z - 122
+    //A - 65 Z - 90
+    //0 - 48 9 - 57
+    //cout<<(int)'9'<<endl;
+    //kody ASCII
 }
 void Zadanie2::run()
 {
